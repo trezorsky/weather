82d58b85709fdb4615833b1e29bb78836952e24b
trezorsky <130697823+trezorsky@users.noreply.github.com>
2023-11-10 19:23:30 +0300

    Update WeatherController.java

--- src/main/java/com/example/weather/controller/WeatherController.java
+++ src/main/java/com/example/weather/controller/WeatherController.java
@@ -6,6 +6,7 @@ import com.example.weather.model.Weather;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
