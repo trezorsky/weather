a41259d8e110cbd663ed4b69b15d79e67a98a650
trezorsky <130697823+trezorsky@users.noreply.github.com>
2023-11-20 12:21:51 +0300

    Update AppConfiguration.java

--- src/main/java/com/example/weather/configuration/AppConfiguration.java
+++ src/main/java/com/example/weather/configuration/AppConfiguration.java
@@ -13,7 +13,6 @@ import org.springframework.web.client.RestTemplate;
 public class AppConfiguration {
 
     @Bean
-    @LoadBalanced
     public  RestTemplate getRestTemplate() {
         return new RestTemplate();
     }
